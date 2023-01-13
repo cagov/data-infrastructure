ca99fa9fad2f106010ddc434f873364ff4883055
Ian Rose <ian.rose@innovation.ca.gov>
2023-01-13 14:17:17 -0800

    Add UDF for generating alphabetical names

--- transform/models/staging/department_of_finance/stg_department_of_finance__entities.sql
+++ transform/models/staging/department_of_finance/stg_department_of_finance__entities.sql
@@ -1,4 +1,68 @@
-{{ config(materialized="ephemeral") }}
+/*
+Wanted to materialize using "ephemeral", but using a temporary
+function requires a materialized table in BQ.
+*/
+{{ config(materialized="table") }}
+
+{% call set_sql_header(config) %}
+create temp function reorder_name_for_alphabetization(name string)
+returns string
+language js
+as
+    r"""
+  // Replace fancy quotes with normal ones.
+  name = name.replace("’", "'");
+
+  // Skip some exceptions
+  const skip = ["Governor's Office"];
+  if (skip.includes(name)) {
+    return name;
+  }
+
+  // Annoying exceptions
+  if (name.includes("Milton Marks") && name.includes("Little Hoover")) {
+    return "Little Hoover Commission";
+  }
+
+  // Basic organizational types by which we don't want to organize.
+  const patterns = [
+    "Office of the Secretary (?:for|of)?",
+    "Commission (?:on|for)?",
+    "Board of Governors (?:for|of)?",
+    "Board (?:of|on|for)?",
+    "Agency (?:on|for)?",
+    "(?:Department|Dept\\.) of",
+    "Commision (?:on|for)?",
+    "Committee (?:on|for)?",
+    "Bureau of",
+    "Council on",
+    "Policy Council on",
+    "Institute of",
+    "Office (?:for|of)?",
+    "Secretary (?:for|of)?",
+    "", // Empty pattern to catch the prefixes below.
+  ].map(
+    // Lots of entities also start with throat clearing like "California this"
+    // or "State that", which we also want to skip. Some also include a definite
+    // article after the organizational unit.
+    (p) =>
+      "(?:California\\s+)?(?:Governor's\\s+)?(?:State\\s+|St\\.\\s+)?(?:Intergovernmental\\s+)?" +
+      p +
+      "(?:\\s+the)?"
+  );
+
+  const all_patterns = `(${patterns.join("|")})`;
+  const re = RegExp(`^${all_patterns}\\s*(.+)$`); // \s* because some of the above eat spaces.
+  const match = name.match(re);
+  // Empty prefixes are matched, so skip if we don't get a full match.
+  if (match && match[1] && match[2]) {
+    return `${match[2].trim()}, ${match[1].trim()}`;
+  } else {
+    return name;
+  }
+"""
+;
+{%- endcall %}
 
 with
     base_entities as (select * from {{ source("state_entities", "base_entities") }}),
@@ -35,7 +99,11 @@ with
             regexp_extract(name, r"[A-Z/]+ USE ONLY") as restricted_use,
             name as name_raw
         from base_entities
+    ),
+
+    entities_with_alphabetization as (
+        select *, reorder_name_for_alphabetization(name) as name_alpha from entities
     )
 
 select *
-from entities
+from entities_with_alphabetization
