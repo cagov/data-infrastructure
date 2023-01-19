58c4429e031bd916f80b36093bc9a402b622ee85
Ian Rose <ian.rose@innovation.ca.gov>
2023-01-19 09:13:38 -0800

    Joining budgets on primary BU code

---
+++ transform/models/intermediate/state_entities/int_state_entities__budgets.sql
@@ -0,0 +1,21 @@
+
+{{ config(materialized="view") }}
+
+with
+    active_entities as (select * from {{ ref("int_state_entities__active") }}),
+
+    budgets as (select * from {{ ref("stg_ebudget__budgets") }}),
+
+    active_entity_budgets as (
+        select
+            active_entities.primary_code,
+            active_entities.name,
+            active_entities.name_alpha,
+            budgets.budget_year_dollars
+        from active_entities
+        left join budgets on active_entities.primary_code = budgets.primary_code
+    )
+
+select *
+from active_entity_budgets
+order by primary_code asc
