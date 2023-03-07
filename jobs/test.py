70d7213377ffeea209fd003aa27c07a2a52e4652
Ian Rose <ian.rose@innovation.ca.gov>
2023-03-07 12:54:51 -0800

    Try adding s3 I/O to execution role

--- jobs/test.py
+++ jobs/test.py
@@ -1,4 +1,8 @@
 from __future__ import annotations
+import geopandas
+
+URL = "https://usbuildingdata.blob.core.windows.net/usbuildings-v2/Alaska.geojson.zip"
 
 if __name__ == "__main__":
-    print("Hello, world")
+    gdf = geopandas.read_file(URL)
+    gdf.to_parquet("s3://dse-infra-dev-scratch/alaska.parquet")
