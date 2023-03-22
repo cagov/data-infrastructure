d9911a9a0e3e3cc07467276e2f7ea782e838aee9
Ian Rose <ian.rose@innovation.ca.gov>
2023-03-22 09:08:50 -0700

    Get working in separate vpc

--- terraform/airflow.tf
+++ terraform/airflow.tf
@@ -1,14 +1,22 @@
+resource "aws_vpc" "mwaa" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
 module "airflow" {
   source  = "idealo/mwaa/aws"
   version = "v2.2.0"
 
   account_id       = data.aws_caller_identity.current.account_id
   environment_name = "${var.name}-mwaa-environment"
+  airflow_version  = "2.4.3"
   region           = var.region
 
-  create_networking_config = false
-  vpc_id                   = aws_vpc.this.id
-  private_subnet_ids       = aws_subnet.private[*].id
+  create_networking_config = true
+  vpc_id                   = aws_vpc.mwaa.id
+  private_subnet_cidrs     = ["10.0.1.0/24", "10.0.2.0/24"] # depending on your vpc ip range
+  public_subnet_cidrs      = ["10.0.3.0/24", "10.0.4.0/24"] # depending on your vpc ip range
+  webserver_access_mode    = "PUBLIC_ONLY"
 
   source_bucket_arn = aws_s3_bucket.mwaa.arn
 }
