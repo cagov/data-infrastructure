4dee687dd856acc34dd463bcaadd7028a60bf850
Ian Rose <ian.rose@innovation.ca.gov>
2023-03-23 13:44:32 -0700

    Add batch submission to mwaa execution role

--- terraform/batch.tf
+++ terraform/batch.tf
@@ -124,6 +124,12 @@ data "aws_iam_policy_document" "batch_submit_policy_document" {
       aws_batch_job_queue.batch_queue.arn,
     ]
   }
+  statement {
+    actions = [
+      "batch:DescribeJobs",
+    ]
+    resources = ["*"]
+  }
 }
 
 resource "aws_iam_policy" "batch_submit_policy" {
