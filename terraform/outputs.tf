6ecc1ab9e207022a6bd23b33ad109519ef69634c
Ian Rose <ian.rose@innovation.ca.gov>
2023-03-23 11:11:42 -0700

    Include MWAA S3 bucket

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,7 @@ output "state" {
   value = {
     repository_url       = aws_ecr_repository.main_ecr.repository_url
     scratch_bucket       = aws_s3_bucket.scratch.id
+    mwaa_bucket          = aws_s3_bucket.mwaa.id
     github_actions_bot   = aws_iam_user.cd_bot.name
     batch_job_queue      = aws_batch_job_queue.batch_queue.name
     batch_job_definition = aws_batch_job_definition.batch_job_def.name
