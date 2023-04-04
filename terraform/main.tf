19ddcf9e56691e24651434981fb2cae997e4bb9c
Ian Rose <ian.rose@innovation.ca.gov>
2023-04-04 14:58:17 -0700

    Update lockfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,11 @@ terraform {
   backend "s3" {
   }
 
+  # Note: when a package is added or updated, we have to update the lockfile in a
+  # platform-independent way, cf. https://github.com/hashicorp/terraform/issues/28041
+  # To update the lockfile run:
+  #
+  # terraform providers lock -platform=linux_amd64 -platform=darwin_amd64
   required_providers {
     aws = {
       source  = "hashicorp/aws"
