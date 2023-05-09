d52469ff72e275ad2a256d46ba46f31383c77b7c
Ian Rose <ian.rose@innovation.ca.gov>
2023-05-09 08:53:54 -0700

    Provider information back in remote-state pseudo-module, use a symlink

--- terraform/s3-remote-state/main.tf
+++ terraform/s3-remote-state/main.tf
@@ -1,3 +1,64 @@
+################################
+#          Variables           #
+################################
+
+variable "owner" {
+  description = "Owner of the resource"
+  type        = string
+  default     = "dse"
+}
+
+variable "project" {
+  description = "Name of the project the resource is serving"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment of the resource"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-west-2"
+}
+
+locals {
+  prefix = "${var.owner}-${var.project}-${var.environment}"
+}
+
+################################
+#       Terraform setup        #
+################################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.56.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      Owner       = var.owner
+      Project     = var.project
+      Environment = var.environment
+    }
+  }
+}
+
+
+################################
+#        State backend         #
+################################
+
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "${local.prefix}-terraform-state"
 
@@ -24,4 +85,28 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
     name = "LockID"
     type = "S"
   }
+}
+
+################################
+# Outputs for a tfbackend file #
+################################
+
+output "bucket" {
+  description = "State bucket"
+  value       = aws_s3_bucket.terraform_state.bucket
+}
+
+output "key" {
+  description = "State object key"
+  value       = "${local.prefix}.tfstate"
+}
+
+output "region" {
+  description = "AWS Region"
+  value       = var.region
+}
+
+output "dynamodb_table" {
+  description = "State lock"
+  value       = aws_dynamodb_table.terraform_state_lock.name
 }
