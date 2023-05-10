125e9f3a50103d32d0b9e170591ba3b468b04d3a
Ian Rose <ian.rose@innovation.ca.gov>
2023-05-10 13:57:45 -0700

    Don't prefix custom schema names with target schema. Instead, we rely on dev/prod separation. If engineers start to step on each others toes in dev, we can revisit and start prefixing those.

---
+++ transform/macros/get_custom_schema.sql
@@ -0,0 +1,21 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+{#
+        Definitions:
+            - custom_schema_name: schema provided via dbt_project.yml or model config
+            - target.name: name of the target (dev for local development, prod for production, etc.)
+            - target.schema: schema provided by the target defined in profiles.yml
+
+        Rather than write to a schema prefixed with target.schema, we instead just write
+        to the actual schema name, and get safety by separating dev and prod databases.
+        If we start to experience analytics engineers stepping on each others toes in
+        dev, we may want to restore prefixes there (while maintaining a prefix-free
+        lifestyle in prod).
+    #}
+{%- if custom_schema_name is none -%} {{ target.schema.lower() | trim }}
+
+{%- else -%} {{ custom_schema_name.lower() | trim }}
+
+{%- endif -%}
+
+{%- endmacro %}
