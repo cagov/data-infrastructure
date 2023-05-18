39c6198d6c5dcc663e09ab1615dd87b9aa4b5681
Ian Rose <ian.rose@innovation.ca.gov>
2023-05-17 17:39:47 -0700

    WIP updating building footprints job to load to snowflake

--- jobs/geo/building_footprints.py
+++ jobs/geo/building_footprints.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from jobs.utils.snowflake import gdf_to_snowflake
+from jobs.utils.snowflake import gdf_to_snowflake, snowflake_connection_from_environment
 
 
 def load_state_footprints(conn) -> None:
@@ -9,31 +9,21 @@ def load_state_footprints(conn) -> None:
 
     print("Downloading data")
     gdf = geopandas.read_file(
-        "https://usbuildingdata.blob.core.windows.net/usbuildings-v2/California.geojson.zip"
+        "https://usbuildingdata.blob.core.windows.net/usbuildings-v2/Alaska.geojson.zip"
     )
 
     print("Writing data to snowflake")
     gdf_to_snowflake(
         gdf,
         conn,
-        table_name="CALIFORNIA_BUILDING_FOOTPRINTS",
+        table_name="ALASKA_BUILDING_FOOTPRINTS",
         cluster=False,
     )
 
 
 if __name__ == "__main__":
-    import os
-
-    import snowflake.connector
-
-    conn = snowflake.connector.connect(
-        account=os.environ["SNOWFLAKE_ACCOUNT"],
-        user=os.environ["SNOWFLAKE_USER"],
-        password=os.environ["SNOWFLAKE_PASSWORD"],
-        warehouse=os.environ["SNOWFLAKE_WAREHOUSE"],
-        database=os.environ["SNOWFLAKE_DATABASE"],
+    conn = snowflake_connection_from_environment(
         schema="GEO_REFERENCE",
-        role=os.environ["SNOWFLAKE_ROLE"],
         client_session_keep_alive=True,  # This can be a slow job! Keep the session alive
     )
     load_state_footprints(conn)
