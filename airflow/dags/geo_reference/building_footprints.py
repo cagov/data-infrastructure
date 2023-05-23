4214b024ec1029614a2d2eddeae21b7ad4bc0839
Ian Rose <ian.rose@innovation.ca.gov>
2023-05-23 07:58:24 -0700

    Cleanup

--- airflow/dags/geo_reference/building_footprints.py
+++ airflow/dags/geo_reference/building_footprints.py
@@ -13,16 +13,16 @@ from airflow.providers.amazon.aws.sensors.batch import BatchSensor
 
 @dag(
     description="Test DAG",
-    start_date=datetime(2023, 3, 23),
-    schedule_interval="@daily",
+    start_date=datetime(2023, 5, 23),
+    schedule_interval="@monthly",
     default_args=DEFAULT_ARGS,
     catchup=False,
 )
 def building_footprints_dag():
     """Test DAG."""
     submit_batch_job = BatchOperator(
-        task_id="submit_batch_job",
-        job_name="submit_batch_job",
+        task_id="load_footprints",
+        job_name="california_building_footprints",
         job_queue=os.environ["AIRFLOW__CUSTOM__DEFAULT_JOB_QUEUE"],
         job_definition=os.environ["AIRFLOW__CUSTOM__DEFAULT_JOB_DEFINITION"],
         overrides={
