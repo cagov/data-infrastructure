40d5e744f251e07dab279c2e9c66b79957c1b504
Ian Rose <ian.rose@innovation.ca.gov>
2023-05-25 10:09:58 -0700

    Dynamically construct task groups for state geoportal data.

--- jobs/geo/core.py
+++ jobs/geo/core.py
@@ -5,17 +5,21 @@ from io import StringIO
 from jobs.utils.snowflake import gdf_to_snowflake, snowflake_connection_from_environment
 
 
-def load_city_county_data(conn, url: str, table: str) -> None:
-    """Load California county data. Given a URL, load geospatial data into BigQuery."""
+def load_geo_data(conn, url: str, table: str) -> None:
+    """Load general geospatial data. Given a URL, load geospatial data into Snowflake."""
     import geopandas
     import requests
 
-    print("Downloading data")
+    print(f"Downloading data {table} from {url}")
 
+    # Some of the target sources come from Esri servers, and GDAL's vsi system
+    # can be cranky about reading from them (issues with range requests and other
+    # weird errors). So instead we load the data first into a text blob using
+    # requests, then hand it off to geopandas.
     f = StringIO(requests.get(url).text)
     gdf = geopandas.read_file(f)
 
-    print("Writing data to snowflake")
+    print(f"Writing {table} to snowflake")
     gdf_to_snowflake(
         gdf,
         conn,
@@ -30,7 +34,7 @@ if __name__ == "__main__":
         database="RAW_DEV",
         schema="GEO_REFERENCE",
         role="LOADER_DEV",
-        client_session_keep_alive=True,  # This can be a slow job! Keep the session alive
+        client_session_keep_alive=True,
     )
 
     import sys
@@ -40,4 +44,4 @@ if __name__ == "__main__":
     assert (
         len(sys.argv) == N_ARGS
     ), "Expecting exactly two arguments: URL and table name."
-    load_city_county_data(conn, sys.argv[1], sys.argv[2])
+    load_geo_data(conn, sys.argv[1], sys.argv[2])
