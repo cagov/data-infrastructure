3eb7336aea0d70eecad92bb5638b2e45b535c6cc
britt <47340362+traintestbritt@users.noreply.github.com>
2023-07-20 11:20:57 -0700

    refactored geo loading script + added pip and pygris

--- jobs/geo/core.py
+++ jobs/geo/core.py
@@ -1,27 +1,25 @@
 from __future__ import annotations
 
-from io import StringIO
-
 from jobs.utils.snowflake import gdf_to_snowflake, snowflake_connection_from_environment
 
 
-def load_geo_data(conn, url: str, table: str) -> None:
+def load_geo_data(conn, state: str, year: str, table: str) -> None:
     """Load general geospatial data. Given a URL, load geospatial data into Snowflake."""
-    import geopandas
-    import requests
+    import pygris
+    from pygris import counties
 
-    print(f"Downloading data {table} from {url}")
+    print(f"Downloading data {table} for {state} in year {year}")
 
     # Some of the target sources come from Esri servers, and GDAL's vsi system
     # can be cranky about reading from them (issues with range requests and other
     # weird errors). So instead we load the data first into a text blob using
     # requests, then hand it off to geopandas.
-    f = StringIO(requests.get(url).text)
-    gdf = geopandas.read_file(f)
+    
+    c = counties(state = state, year = year, cache = True)
 
     print(f"Writing {table} to snowflake")
     gdf_to_snowflake(
-        gdf,
+        c,
         conn,
         table_name=table,
         cluster=False,
@@ -37,8 +35,8 @@ if __name__ == "__main__":
     import sys
 
     # TODO: perhaps make a real CLI here.
-    N_ARGS = 3
+    N_ARGS = 4
     assert (
         len(sys.argv) == N_ARGS
-    ), "Expecting exactly two arguments: URL and table name."
-    load_geo_data(conn, sys.argv[1], sys.argv[2])
+    ), "Expecting exactly 3 arguments: two letter state abbreviation, four digit year, and table name."
+    load_geo_data(conn, sys.argv[1], sys.argv[2], sys.argv[3])
