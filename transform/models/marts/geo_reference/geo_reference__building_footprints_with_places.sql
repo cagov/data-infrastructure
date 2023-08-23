0674e4f0f2ce5eff98ee511b61607c62d26e61e6
Ian Rose <ian.rose@innovation.ca.gov>
2023-08-23 09:39:29 -0700

    Apply deduplication logic to footprints-with-places model

--- transform/models/marts/geo_reference/geo_reference__building_footprints_with_places.sql
+++ transform/models/marts/geo_reference/geo_reference__building_footprints_with_places.sql
@@ -2,7 +2,11 @@ with footprints as (
     select
         "release",
         "capture_dates_range",
-        "geometry"
+        "geometry",
+        /* Generate a temporary ID for footprints. We will need this to group/partition
+        by unique footprints further down. We could use a UUID, but integers are
+        cheaper to generate and compare. */
+        seq4() as _tmp_id
     from {{ source('building_footprints', 'california_building_footprints') }}
 ),
 
@@ -25,9 +29,33 @@ places as (
 footprints_and_places_joined as (
     select
         footprints.*,
-        places.* exclude "geometry"
+        places.* exclude "geometry",
+        /* We don't actually need the intersection for every footprint, only for the
+         ones that intersect more than one place. However, in order to establish which
+         ones intersect more than one place, we need a windowed COUNT partitioned by
+         _tmp_id. This is an expensive operation, as it likely triggers a shuffle
+         (even though it should already be sorted by _tmp_id). In testing we've found
+         that it's cheaper to just do the intersection for all the footprints. */
+        st_area(st_intersection(footprints."geometry", places."geometry"))
+            as _tmp_intersection
     from footprints
     left join places on st_intersects(footprints."geometry", places."geometry")
+),
+
+footprints_and_places_joined_dedupe as (
+    select
+        -- Snowflake doesn't support geometries in max_by. It should, but it doesn't.
+        -- Fortunately, we know that the geometries are identical when partitioned
+        -- by _tmp_id, so we can just choose any_value.
+        any_value("geometry") as "geometry",
+        max_by("place_fp", _tmp_intersection) as "place_fp",
+        max_by("place_ns", _tmp_intersection) as "place_ns",
+        max_by("name", _tmp_intersection) as "name",
+        max_by("geoid", _tmp_intersection) as "geoid",
+        max_by("class_fips_code", _tmp_intersection) as "class_fips_code",
+        max_by("class_fips", _tmp_intersection) as "class_fips"
+    from footprints_and_places_joined
+    group by _tmp_id
 )
 
-select * from footprints_and_places_joined
+select * from footprints_and_places_joined_dedupe
