cfb7a09013569911d2d6d2e3e4a1f1e321ec3ea9
britt <47340362+traintestbritt@users.noreply.github.com>
2023-09-01 11:40:22 -0700

    refactored writing script, may be final

--- jobs/geo/write_building_footprints.py
+++ jobs/geo/write_building_footprints.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
-import s3fs
-
 from jobs.utils.snowflake import snowflake_connection_from_environment
 
 
 def write_building_footprints(conn):
-    """Grab Microsoft state building footprint data for California from Snowflake and write to an S3 bucket."""
+    """Grab Microsoft building footprints data enriched with Census TIGER Blocks data for California from Snowflake and write to an S3 bucket."""
+    import os
+
     import geopandas
+    import s3fs
     import shapely
 
     sql_alter = """
@@ -18,12 +19,13 @@ def write_building_footprints(conn):
     counties = """
     SELECT DISTINCT "county_fips"
     FROM ANALYTICS_DEV.ANALYTICS.GEO_REFERENCE__BUILDING_FOOTPRINTS_WITH_BLOCKS
+    ORDER BY 1 ASC
     """
 
     counties = conn.cursor().execute(counties).fetchall()
     counties = [x[0] for x in counties if x[0] is not None]
 
-    for county in counties:
+    for index, county in enumerate(counties):
         sql_table = f"""
         SELECT *
         FROM ANALYTICS_DEV.ANALYTICS.GEO_REFERENCE__BUILDING_FOOTPRINTS_WITH_BLOCKS
@@ -33,24 +35,28 @@ def write_building_footprints(conn):
         gdf = geopandas.GeoDataFrame(
             df.assign(geometry=df.geometry.apply(shapely.wkb.loads))
         )
-        gdf.to_parquet(f"footprints_with_blocks_for_county_fips_{county}.parquet")
-        gdf.to_file(
-            f"footprints_with_blocks_for_county_fips_{county}.shp.zip",
-            driver="ESRI Shapefile",
+
+        gdf = gdf[gdf.geometry.geom_type != "GeometryCollection"]
+
+        file_prefix = f"footprints_with_blocks_for_county_fips_{county}"
+        gdf.to_parquet(f"{file_prefix}.parquet")
+        gdf.to_file(f"{file_prefix}.shp.zip")
+
+        print(
+            f"Loading {file_prefix}. This is number {index+1} out of {len(counties)} counties."
         )
-        print(f"loading footprints_with_blocks_for_county_fips_{county}")
 
         s3 = s3fs.S3FileSystem(anon=False)
         s3.put(
-            f"footprints_with_blocks_for_county_fips_{county}.parquet",
-            "dof-demographics-dev-us-west-2-public/",
-            recursive=True,
+            f"{file_prefix}.parquet",
+            "s3://dof-demographics-dev-us-west-2-public/parquet/",
         )
         s3.put(
-            f"footprints_with_blocks_for_county_fips_{county}.shp.zip",
-            "dof-demographics-dev-us-west-2-public/",
-            recursive=True,
+            f"{file_prefix}.shp.zip", "s3://dof-demographics-dev-us-west-2-public/shp/"
         )
+
+        os.remove(f"{file_prefix}.parquet")
+        os.remove(f"{file_prefix}.shp.zip")
 
 
 if __name__ == "__main__":
