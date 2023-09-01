c48cfb768758be66f43d039ea3281105370e17d4
Ian Rose <ian.rose@innovation.ca.gov>
2023-09-01 08:58:14 -0700

    Add customizable prefix to temporary CTEs so it can be used more than once

--- transform/macros/spatial_join_with_deduplication.sql
+++ transform/macros/spatial_join_with_deduplication.sql
@@ -1,6 +1,6 @@
-{% macro spatial_join_with_deduplication(left_model, right_model, left_cols, right_cols, left_geom="geometry", right_geom="geometry", op="st_intersects", kind="left") %}
+{% macro spatial_join_with_deduplication(left_model, right_model, left_cols, right_cols, left_geom="geometry", right_geom="geometry", op="st_intersects", kind="left", prefix="") %}
 
-with left_model_with_id as (
+with {{ prefix }}_left_model_with_id as (
     select
         /* Generate a temporary ID for footprints. We will need this to group/partition
         by unique footprints further down. We could use a UUID, but integers are
@@ -9,15 +9,15 @@ with left_model_with_id as (
     from {{ left_model }}
 ),
 
-joined as (
+{{ prefix }}_joined as (
     select
       {% for lcol in left_cols %}
-      left_model_with_id.{{ lcol }},
+      {{ prefix }}_left_model_with_id.{{ lcol }},
       {% endfor %}
       {% for rcol in right_cols %}
       {{ right_model }}.{{ rcol }},
       {% endfor %}
-      left_model_with_id.{{ left_geom }},
+      {{ prefix }}_left_model_with_id.{{ left_geom }},
       /* We don't actually need the intersection for every geometry, only for the
        ones that intersect more than one. However, in order to establish which
        ones intersect more than one, we need a windowed COUNT partitioned by
@@ -25,29 +25,29 @@ joined as (
        (even though it should already be sorted by _tmp_id). In testing we've found
        that it's cheaper to just do the intersection for all the geometries. */
       st_area(
-        st_intersection(left_model_with_id.{{ left_geom }}, {{ right_model }}.{{ right_geom }})
+        st_intersection({{ prefix }}_left_model_with_id.{{ left_geom }}, {{ right_model }}.{{ right_geom }})
       ) as _tmp_sjoin_intersection,
-      left_model_with_id._tmp_sjoin_id
-    from left_model_with_id
+      {{ prefix }}_left_model_with_id._tmp_sjoin_id
+    from {{ prefix }}_left_model_with_id
     {{ kind }} join {{ right_model }}
-    on {{ op }}(left_model_with_id.{{ left_geom }}, {{ right_model }}.{{ right_geom }})
+    on {{ op }}({{ prefix }}_left_model_with_id.{{ left_geom }}, {{ right_model }}.{{ right_geom }})
 ),
 
-deduplicated as (
+{{ prefix }}_deduplicated as (
     select
       -- Snowflake doesn't support geometries in max_by. It should, but it doesn't.
       -- Fortunately, we know that the geometries are identical when partitioned
       -- by _tmp_sjoin_id, so we can just choose any_value.
-      any_value({{ left_geom }}),
+      any_value({{ left_geom }}) as {{ left_geom }},
       {% for lcol in left_cols %}
       max_by({{ lcol }}, _tmp_sjoin_intersection) as {{ lcol }},
       {% endfor %}
       {% for rcol in right_cols %}
       max_by({{ rcol }}, _tmp_sjoin_intersection) as {{ rcol }}{{ "," if not loop.last }}
       {% endfor %}
-    from joined
+    from {{ prefix }}_joined
     group by _tmp_sjoin_id
 )
 
-select * from deduplicated
+select * from {{ prefix }}_deduplicated
 {%- endmacro -%}
