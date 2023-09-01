e8e03748a544622a9655695e97f7c7fec4d0ae61
Ian Rose <ian.rose@innovation.ca.gov>
2023-09-01 10:17:39 -0700

    Refactor to include places and blocks in the same enriched model

--- transform/models/marts/geo_reference/geo_reference__building_footprints_with_blocks.sql
+++ transform/models/marts/geo_reference/geo_reference__building_footprints_with_blocks.sql
@@ -1,3 +1,23 @@
+{%- set footprint_cols = ['"release"', '"capture_dates_range"'] -%}
+
+{%- set block_cols = {
+      '"COUNTYFP20"': '"county_fips"',
+      '"TRACTCE20"': '"tract"',
+      '"BLOCKCE20"': '"block"',
+      '"GEOID20"': '"block_geoid"',
+      '"NAME20"': '"block_name"',
+  }
+-%}
+
+{%- set place_cols = {
+     '"PLACEFP"': '"place_fp"',
+     '"PLACENS"': '"place_ns"',
+     '"GEOID"': '"place_geoid"',
+     '"NAME"': '"place_name"',
+     '"CLASSFP"': '"class_fips_code"',
+  }
+-%}
+
 with footprints as (
     select * from {{ source('building_footprints', 'california_building_footprints') }}
 ),
@@ -6,26 +26,51 @@ blocks_source as (
     select * from {{ source('tiger_2022', 'blocks') }}
 ),
 
+places_source as (
+    select * from {{ source('tiger_2022', 'places') }}
+),
+
 blocks as (
     select
-        "COUNTYFP20" as "county_fips",
-        "TRACTCE20" as "tract",
-        "BLOCKCE20" as "block",
-        "GEOID20" as "geoid",
-        "NAME20" as "name",
+        {% for k, v in block_cols.items() %}
+            {{ k }} as {{ v }},
+        {% endfor %}
         "geometry"
     from blocks_source
 ),
 
-footprints_and_blocks_joined as (
+places as (
+    select
+        {% for k, v in place_cols.items() %}
+            {{ k }} as {{ v }},
+        {% endfor %}
+        {{ map_class_fips("CLASSFP") }} as "class_fips",
+        "geometry"
+    from places_source
+),
+
+footprints_with_blocks as (
     {{ spatial_join_with_deduplication(
        "footprints",
        "blocks",
-       ['"release"', '"capture_dates_range"'],
-       ['"county_fips"', '"tract"', '"block"', '"geoid"', '"name"'],
+       footprint_cols,
+       block_cols.values(),
        left_geom='"geometry"',
        right_geom='"geometry"',
+       prefix="b",
+    ) }}
+),
+
+footprints_with_blocks_and_places as (
+    {{ spatial_join_with_deduplication(
+       "footprints_with_blocks",
+       "places",
+       footprint_cols + block_cols.values() | list,
+       place_cols.values() | list + ['"class_fips"'],
+       left_geom='"geometry"',
+       right_geom='"geometry"',
+       prefix="p",
     ) }}
 )
 
-select * from footprints_and_blocks_joined
+select * from footprints_with_blocks_and_places
