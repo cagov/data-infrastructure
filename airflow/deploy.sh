be308e05651552fd8b09e7607942b57c8c9c1467
Ian Rose <ian.rose@innovation.ca.gov>
2023-11-14 14:18:58 -0800

    Add s3fs and fsspec to requirements, be less aggressive about deleting code from the dags so that other repos can also add dags.

--- airflow/deploy.sh
+++ airflow/deploy.sh
@@ -4,7 +4,12 @@ BUCKET="dse-infra-dev-us-west-2-mwaa"
 ENVIRONMENT="dse-infra-dev-us-west-2-mwaa-environment"
 
 # Copy the dags
-aws s3 sync dags s3://${BUCKET}/dags --delete --exclude "*__pycache__*"
+for SUBDIR in $(ls dags); do
+  if [ $SUBDIR == '__pycache__' ]; then
+      continue
+  fi
+  aws s3 sync dags/$SUBDIR s3://${BUCKET}/dags/$SUBDIR --delete --exclude "*__pycache__*"
+done
 
 # Copy the requirements.txt
 OLD_VERSION=$(aws mwaa get-environment --name $ENVIRONMENT --query 'Environment.RequirementsS3ObjectVersion' --output text)
