e1b91c9733d1541b3c280fbbbf5318a2e9a36fe4
britt <47340362+traintestbritt@users.noreply.github.com>
2024-08-21 15:19:39 -0700

    removed unused CTEs from files

--- transform/models/marts/geo_reference/geo_reference__global_ml_building_footprints_with_tiger.sql
+++ transform/models/marts/geo_reference/geo_reference__global_ml_building_footprints_with_tiger.sql
@@ -1,52 +1,10 @@
-with footprints as (
-    select
-        "height",
-        "geometry"
-    from {{ source('building_footprints', 'global_ml_building_footprints') }}
-),
-
-blocks_source as (
+with blocks_source as (
     select *
     from {{ source('tiger_2022', 'blocks') }}
 ),
 
 places_source as (
     select * from {{ source('tiger_2022', 'places') }}
-),
-
-blocks as (
-    select
-        "COUNTYFP20" as "county_fips",
-        "TRACTCE20" as "tract",
-        "BLOCKCE20" as "block",
-        "GEOID20" as "block_geoid",
-        "geometry"
-    from blocks_source
-),
-
-places as (
-    select
-        "PLACEFP" as "place_fips",
-        "PLACENS" as "place_ns",
-        "GEOID" as "place_geoid",
-        "NAME" as "place_name",
-        "CLASSFP" as "class_fips_code",
-        {{ map_class_fips("CLASSFP") }} as "class_fips",
-        "geometry"
-    from places_source
-),
-
-footprints_with_blocks as (
-    {{ spatial_join_with_deduplication(
-       "footprints",
-       "blocks",
-       ['"height"'],
-       ['"county_fips"', '"tract"', '"block"', '"block_geoid"'],
-       left_geom='"geometry"',
-       right_geom='"geometry"',
-       kind="inner",
-       prefix="b",
-    ) }}
 ),
 
 footprints_with_blocks_and_places as (
