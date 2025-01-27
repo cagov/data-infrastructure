24230f892c07495e0674f712544e4eae01250ea0
Ian Rose <ian.rose@innovation.ca.gov>
2025-01-27 15:27:42 -0800

    Rework staging models to be incremental with some light aggregation. A few notes: 1. Incremental because the cost tracking views in the SNOWFLAKE database    have a retention time of one year. This allows us to store the data    for longer while continuing to get updates 2. We turn off full_refresh of these incremental models to avoid    accidentally trashing old data that is beyond the retention period 3. No reason to have a small lookback period for incrementality: these    are small data. 4. We do some light aggregation in the models to ensure the rows of the    tables are really unique for a given usage date. A few of the tables    had some rows with zero usage that were duplicated, and the database    doesn't really make any strong guarantees around uniqueness.

--- transform/models/staging/snowflake_cost_tracking/stg_database_storage_usage_history.sql
+++ transform/models/staging/snowflake_cost_tracking/stg_database_storage_usage_history.sql
@@ -1,3 +1,14 @@
+{{ config(
+  materialized="incremental",
+  unique_key=[
+    "ORGANIZATION_NAME",
+    "ACCOUNT_NAME",
+    "DATABASE_NAME",
+    "USAGE_DATE",
+    ],
+  )
+}}
+
 WITH source AS (
     SELECT
         region,
@@ -15,8 +26,16 @@ WITH source AS (
 ),
 
 database_storage_usage_history AS (
-    SELECT *
+    SELECT
+        organization_name,
+        account_name,
+        database_name,
+        usage_date,
+        AVG(average_hybrid_table_storage_bytes) AS average_hybrid_table_storage_bytes,
+        AVG(average_database_bytes) AS average_database_bytes,
+        AVG(average_failsafe_bytes) AS average_failsafe_bytes
     FROM source
+    GROUP BY ALL
 )
 
 SELECT *
