9ee9738f285d2b478822cf5b32bde82db9deb190
Ian Rose <ian.rose@innovation.ca.gov>
2025-04-02 12:34:57 -0700

    Sideport sqlfluff_pre_commit script

---
+++ sqlfluff_pre_commit.py
@@ -0,0 +1,32 @@
+# sqlfluff_pre_commit.py
+import pathlib
+import subprocess
+import sys
+
+
+def main():
+    fnames = [str(pathlib.Path(*pathlib.Path(f).parts[1:])) for f in sys.argv[1:]]
+    try:
+        subprocess.run(
+            [
+                "sqlfluff",
+                "fix",
+                "--show-lint-violations",
+                "--processes",
+                "0",
+                "--nocolor",
+                "--disable-progress-bar",
+                "--force",
+                *fnames,
+            ],
+            capture_output=True,
+            check=True,
+            cwd="transform",
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"SQLFluff found linting errors:\n{e.stdout.decode()}")
+        sys.exit(e.returncode)
+
+
+if __name__ == "__main__":
+    main()
